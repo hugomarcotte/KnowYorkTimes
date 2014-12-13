a408af67e5303802b1a6e194a19aafe9bb27d9b6
Hugo Marcotte <marcotte.hugo@gmail.com>
2014-12-13 15:10:23 -0500

    Fix layout and background

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,45 +2,32 @@
 
 angular.module('knowyorktimesApp')
   .controller('MainCtrl', function ($scope, $http, $mdToast, $animate, $interval) {
-    $scope.awesomeThings = [];
+    var secondsLeft = 120;
 
+    $scope.score = 0;
+    $scope.quizStarted = false;
 
     $scope.startQuiz = function() {
       $http.get('/api/questions').success(function(questions) {
-        console.log(questions);
         $scope.questions = questions;
+        $scope.quizStarted = true;
+        $scope.countDown();
       });
-      $scope.countDown();
     }
 
-
-    $scope.score = 0;
-    $scope.secondsLeftForBar = 120;
-
-
-    var secondsLeft = 120;
-    var secondsLeftForBar = 120;
     $scope.countDown = function() {
       $interval(function() {
-        secondsLeftForBar--;
         secondsLeft--;
         $scope.minLeft = Math.floor(secondsLeft / 60);
         $scope.secondsLeft = secondsLeft % 60;
-        $scope.secondsLeftForBar = secondsLeftForBar;
         if ($scope.minLeft === 0 && $scope.secondsLeft === 0) {
           $scope.$apply();
           alert("the quiz is over! your score was " + $scope.score);
         }
       }, 1000, 120);
-
     }
 
-
-
-
-
-
-    $scope.yesButtonClickImageToTitle = function(questionObject) {
+    $scope.answerQuestion = function(questionObject) {
       if (questionObject.displayTitle === questionObject.title) {
         $mdToast.show(
           $mdToast.simple()
@@ -60,31 +47,6 @@ angular.module('knowyorktimesApp')
       }
       currentQuestionIndex += 1;
     }
-
-    $scope.noButtonClickImageToTitle = function(questionObject) {
-      if (questionObject.displayTitle !== questionObject.title) {
-        $mdToast.show(
-          $mdToast.simple()
-          .content('Your Right! + 300')
-          .position('top left')
-          .hideDelay(2000)
-        );
-        $scope.score += 300;
-      } else {
-        $mdToast.show(
-          $mdToast.simple()
-          .content('Your Wrong :( -500')
-          .position('top left')
-          .hideDelay(2000)
-        );
-        $scope.score -= 500;
-      }
-      currentQuestionIndex += 1;
-         $scope.pCTMaxScore = ($scope.score/8000) * 100;
-    }
-
-
-
 
     var currentQuestionIndex = 1;
     $scope.filterQuestion = function(question) {
